0c58a19384853301096bcf5d76eb5f870a0f1c10
christopher white <cmwhite1995@gmail.com>
2018-08-18 22:18:06 -0400

    added operation functions

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,6 +9,81 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    var NumbersOnScreen:Double = 0;
+    var PreviousNumber:Double = 0;
+    var Operation = 0;
+    var PerformingMath = false;
+    
+    @IBAction func numbers(_ sender: UIButton) {
+        if(PerformingMath == true)
+        {
+            label.text = String(sender.tag-1);
+            NumbersOnScreen = Double(label.text!)!
+            PerformingMath = false;
+        }
+        else{
+            label.text = label.text! + String(sender.tag-1)
+            NumbersOnScreen = Double(label.text!)!
+        }
+    }
+    
+    @IBAction func Operations(_ sender: UIButton) {
+        
+        if(label.text != "" && sender.tag != 11 && sender.tag != 16 ){
+            PreviousNumber = Double(label.text!)!
+            if(sender.tag == 12) // Divide
+            {
+                label.text = "/"
+            }
+            else if(sender.tag == 13) // Multiply
+            {
+                label.text = "x"
+            }
+            else if(sender.tag == 14) // Subtract
+            {
+                label.text = "-"
+            }
+            else if(sender.tag == 15) // Addition
+            {
+                label.text = "+"
+            }
+            Operation = sender.tag
+            PerformingMath = true
+            
+        }
+        else if sender.tag == 16
+        {
+            if(Operation == 12)
+            {
+                label.text = String(PreviousNumber / NumbersOnScreen)
+            }
+           else if(Operation == 13)
+            {
+                label.text = String(PreviousNumber * NumbersOnScreen)
+            }
+           else if(Operation == 14)
+            {
+                label.text = String(PreviousNumber - NumbersOnScreen)
+            }
+           else if(Operation == 15)
+            {
+                label.text = String(PreviousNumber + NumbersOnScreen)
+            }
+        }
+        else if(sender.tag == 11)
+        {
+            label.text = ""
+            PreviousNumber = 0
+            NumbersOnScreen = 0
+            Operation = 0
+        }
+    }
+    
+    
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
